0878614f5513005f2e1fb643b7b4445f75a3541f
Matti Jokitulppo <melonmanchan@gmail.com>
2017-03-01 13:40:40 +0200

    Add dummy server

--- server.go
+++ server.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	mainRouter := mux.NewRouter()
+
+	mainRouter.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		rw.Write([]byte("Hello"))
+	})
+
+	server := &http.Server{
+		Handler:      mainRouter,
+		Addr:         ":8080",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Fatal(server.ListenAndServe())
 }
