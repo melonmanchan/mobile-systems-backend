4952c2d94adbf24499c63019b05a3f981b0efd00
Matti Jokitulppo <melonmanchan@gmail.com>
2017-03-15 13:28:12 +0200

    Test user resolving

--- handlers/user.go
+++ handlers/user.go
@@ -5,12 +5,15 @@ import (
 	"net/http"
 
 	"../app"
+	"../models"
+	"../types"
 	"github.com/gorilla/mux"
 )
 
 // UserHandler ...
 func UserHandler(app app.App, r *mux.Router) {
 	r.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("test route")
+		user := r.Context().Value(types.UserKey).(*models.User)
+		log.Print(user)
 	}).Methods("POST")
 }
