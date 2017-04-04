c1e353ab95acc66fcaeaf91280c8e7ded47b5ae6
Matti Jokitulppo <melonmanchan@gmail.com>
2017-04-04 14:18:45 +0000

    Add subjects

---
+++ migrations/1491315045_create_subject.up.sql
@@ -0,0 +1,5 @@
+CREATE TABLE subjects (
+    id bigserial primary key,
+    type varchar(64) NOT NULL
+);
+
---
+++ migrations/1491315241_add_subjects.up.sql
@@ -0,0 +1,7 @@
+INSERT INTO subjects (type) values ('Mathematics');
+INSERT INTO subjects (type) values ('Biology');
+INSERT INTO subjects (type) values ('History');
+INSERT INTO subjects (type) values ('Geography');
+INSERT INTO subjects (type) values ('Physics');
+INSERT INTO subjects (type) values ('Chemistry');
+INSERT INTO subjects (type) values ('Writing');
