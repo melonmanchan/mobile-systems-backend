c9e7de9228b01130e85b4ff3620c1e4360eeb92a
Matti Jokitulppo <melonmanchan@gmail.com>
2017-04-13 15:57:52 +0000

    Fix copy-paste error in migration file

--- migrations/1491916663_create_tutorship.up.sql
+++ migrations/1491916663_create_tutorship.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE tutorships (
     id bigserial primary key,
     tutor_id integer REFERENCES users(id) NOT NULL,
-    tutee_id integer REFERENCES subjects(id) NOT NULL,
+    tutee_id integer REFERENCES users(id) NOT NULL,
     created_at TIMESTAMP DEFAULT current_timestamp,
     unique(tutor_id, tutee_id)
 );
