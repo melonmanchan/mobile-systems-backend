fd508452b0a496bfec3dc915cf409713f1957415
Matti Jokitulppo <melonmanchan@gmail.com>
2017-04-16 14:12:59 +0000

    Add some events related helpers

--- models/events.go
+++ models/events.go
@@ -53,7 +53,77 @@ func (c Client) GetTutorOwnTimes(user *User) ([]Event, error) {
 	err := c.DB.Select(&events, `
 	SELECT events.* FROM events
 	WHERE events.tutor = $1;
-	;`, user.ID)
+	`, user.ID)
+
+	if err != nil {
+		return events, err
+	}
+
+	return events, nil
+}
+
+// RemoveTime ...
+func (c Client) RemoveTime(user *User, event *Event) error {
+	if user.UserType != TutorType {
+		return errors.New("user is not a tutor")
+	}
+
+	if event.TutorID != user.ID {
+		return errors.New("event is not tutored by user")
+	}
+
+	_, err := c.DB.Exec(`
+		DELETE FROM EVENTS
+		WHERE events.id = $1;
+	`, event.ID)
+
+	return err
+}
+
+// GetTutorFreeTimes ...
+func (c Client) GetTutorFreeTimes(tutorID int64) ([]Event, error) {
+	events := []Event{}
+
+	err := c.DB.Select(&events, `
+	SELECT events.* FROM events
+	WHERE events.tutor = $1 AND events.tutee IS NULL;
+	`, tutorID)
+
+	if err != nil {
+		return events, err
+	}
+
+	return events, nil
+}
+
+// ReserveTimeForUser ...
+func (c Client) ReserveTimeForUser(user *User, event *Event) error {
+	if event.TuteeID.Valid {
+		return errors.New("event is already reserver")
+	}
+
+	_, err := c.DB.Exec(`
+		UPDATE events
+		SET tutee = $1 WHERE events.id = $2;
+	`, user.ID, event.ID)
+
+	if err != nil {
+		return err
+	}
+
+	event.TuteeID = null.IntFrom(user.ID)
+
+	return nil
+}
+
+// GetTuteeTimes ...
+func (c Client) GetTuteeTimes(user *User) ([]Event, error) {
+	events := []Event{}
+
+	err := c.DB.Select(&events, `
+	SELECT events.* FROM events
+	WHERE events.tutee IS NOT NULL AND events.tutee = $ 1;
+	`, user.ID)
 
 	if err != nil {
 		return events, err
